2dbbcfb3c4b7219451cdea19142e8554ebece598
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-06-15 20:58:28 +0530

    Delete Spring-boot-Coupon_system directory

--- Spring-boot-Coupon_system/Spring-boot-coupon-demo/src/main/java/com/zensar/coupon/repository/CouponRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zensar.coupon.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zensar.coupon.controller.entity.Coupon;
-
-public interface CouponRepository extends JpaRepository<Coupon, Integer> {
-
-}
