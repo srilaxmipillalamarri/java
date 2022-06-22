4e9303d27b404d22a72bc667c2efe69e43a23460
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-06-22 21:37:56 +0530

    updated actuator and swagger

--- spring-boot-student/src/main/java/com/zensar/controller/StudentController.java
+++ spring-boot-student/src/main/java/com/zensar/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.zensar.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
