ffe92f8f067561dfcf5298777997a08f1cc9a24c
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-06-26 19:16:41 +0530

    Add files via upload

---
+++ Spring-boot-product/src/main/java/com/zensar/ide/entity/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.zensar.ide.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+	private int statusCode;
+	 private String message;
+}
