e5861f7d2b8b19f73a8ba0c2d26d2849ac81fc62
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-07-02 12:21:22 +0530

    Add files via upload

---
+++ spring-boot-product/src/test/java/com/zensar/ide/SpringBootProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensar.ide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
