d9d68f07d4a79dcda7523155382c77c1eb704cb5
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-07-05 15:36:15 +0530

    Update App.java

--- Employee-Department/src/main/java/com/zensar/beans/Employee_Department/App.java
+++ Employee-Department/src/main/java/com/zensar/beans/Employee_Department/App.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.zensar.beans.Employee_Department;
 
 import org.springframework.context.ApplicationContext;
@@ -20,26 +20,3 @@ public class App
     	System.out.println(emp);
     }
 }
-=======
-package com.zensar.beans.Employee_Department;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.zensar.employee.Employee;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-    	
-    	Employee emp = (Employee)context.getBean("emp1",Employee.class);
-    	System.out.println(emp);
-    }
-}
->>>>>>> branch 'main' of https://github.com/srilaxmipillalamarri/java.git
