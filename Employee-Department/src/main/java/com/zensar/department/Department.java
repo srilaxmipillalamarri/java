3071fd2f7afcbbb3428ba1d32de8c83e845f0a63
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-07-05 15:35:47 +0530

    Update Department.java

--- Employee-Department/src/main/java/com/zensar/department/Department.java
+++ Employee-Department/src/main/java/com/zensar/department/Department.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.zensar.department;
 
 public class Department {
@@ -29,35 +28,3 @@ public class Department {
 		return "Department [depId=" + depId + ", depName=" + depName + "]";
 	}
 }
-=======
-package com.zensar.department;
-
-public class Department {
-	private int depId;
-	private String depName;
-	private Department() {
-		super();
-	}
-	private Department(int depId, String depName) {
-		super();
-		this.depId = depId;
-		this.depName = depName;
-	}
-	public int getDepId() {
-		return depId;
-	}
-	public void setDepId(int depId) {
-		this.depId = depId;
-	}
-	public String getDepName() {
-		return depName;
-	}
-	public void setDepName(String depName) {
-		this.depName = depName;
-	}
-	@Override
-	public String toString() {
-		return "Department [depId=" + depId + ", depName=" + depName + "]";
-	}
-}
->>>>>>> branch 'main' of https://github.com/srilaxmipillalamarri/java.git
