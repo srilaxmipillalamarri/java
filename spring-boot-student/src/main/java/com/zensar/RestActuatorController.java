d0e7d31b9305a67c8290dd10dff3d2dcd8c07f1a
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-07-05 15:31:16 +0530

    Update RestActuatorController.java

--- spring-boot-student/src/main/java/com/zensar/RestActuatorController.java
+++ spring-boot-student/src/main/java/com/zensar/RestActuatorController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.zensar;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,21 +15,3 @@ public class RestActuatorController {
 		return "hello world";	}
 
 }
-=======
-package com.zensar;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.zensar.controller.entity.Student;
-
-@RestController
-public class RestActuatorController {
-	@GetMapping("/students")
-	public String student() {
-		
-		System.out.println("hello");
-		return "hello world";	}
-
-}
->>>>>>> branch 'main' of https://github.com/srilaxmipillalamarri/java.git
