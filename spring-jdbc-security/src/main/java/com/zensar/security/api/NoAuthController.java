77d4680e061cd7712b79d53ea04ce790afec8e7f
srilaxmipillalamarri <78467693+srilaxmipillalamarri@users.noreply.github.com>
2022-07-05 10:42:40 +0530

    Update NoAuthController.java

--- spring-jdbc-security/src/main/java/com/zensar/security/api/NoAuthController.java
+++ spring-jdbc-security/src/main/java/com/zensar/security/api/NoAuthController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.zensar.security.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,19 +13,3 @@ public class NoAuthController {
 		return "Hello authenticator";
 	}
 }
-=======
-package com.zensar.security.api;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/moAuth/rest")
-public class NoAuthController {
-   @GetMapping("/sayhi")
-	public String sayHi() {
-		return "Hello authenticator";
-	}
-}
->>>>>>> branch 'main' of https://github.com/srilaxmipillalamarri/java.git
